47df90807d31af7fdcc621a39b587fa9459451de
olankens <173156207+olankens@users.noreply.github.com>
2024-06-28 11:01:16 +0200

    Use local variables

--- aurhogen.sh
+++ aurhogen.sh
@@ -6,9 +6,9 @@
 access_external() {
 
 	if [[ $(uname -s) = Dar* ]]; then
-		storage=$(/bin/sh -c "find /Volumes/*KOBO* -maxdepth 0" 2>/dev/null | sort -r | head -1)
+		local storage=$(/bin/sh -c "find /Volumes/*KOBO* -maxdepth 0" 2>/dev/null | sort -r | head -1)
 	elif [[ $(uname -s) = Lin* ]]; then
-		storage=$(/bin/sh -c "find /media/*KOBO* -maxdepth 0" 2>/dev/null | sort -r | head -1)
+		local storage=$(/bin/sh -c "find /media/*KOBO* -maxdepth 0" 2>/dev/null | sort -r | head -1)
 	fi
 	echo "$storage"
 
@@ -16,7 +16,7 @@ access_external() {
 
 ensure_accuracy() {
 
-	failure="\r\033[91m%s\033[00m\n\n"
+	local failure="\r\033[91m%s\033[00m\n\n"
 	if [[ $(uname -s) != Dar* && $(uname -s) != Lin* ]]; then
 		printf "$failure" "THE ACTUAL SYSTEM IS NOT SUPPORTED." && exit 1
 	elif [[ ! -d $(access_external) ]]; then
@@ -29,7 +29,7 @@ ensure_accuracy() {
 
 remove_unwanted() {
 
-	storage=$(access_external)
+	local storage=$(access_external)
 	if [[ -d $storage ]]; then
 		rm -fr "$storage/.kobo-images"
 		rm -fr "$storage/.Spotlight-V100"
@@ -41,16 +41,16 @@ remove_unwanted() {
 
 update_database() {
 
-	storage="$(access_external)/.kobo/KoboReader.sqlite"
+	local storage="$(access_external)/.kobo/KoboReader.sqlite"
 	if [[ -f $storage ]]; then
-		factors=(
+		local factors=(
 			"00000000-0000-0000-0000-000000000000"
 			"00000000-0000-0000-0000-000000000000"
 			"MyDummyUser@dummy.com"
 			"MyDummyUser@dummy.com"
 			"000011"
 		)
-		content=$(printf ",'%s'" "${factors[@]}" | cut -c2-)
+		local content=$(printf ",'%s'" "${factors[@]}" | cut -c2-)
 		echo "INSERT INTO user values ($content);" | sqlite3 "$storage" 2>/dev/null
 	fi
 
@@ -58,7 +58,7 @@ update_database() {
 
 update_firmware() {
 
-	factors=(
+	local factors=(
 		"https://kfwproxy.geek1011.net/api.kobobooks.com?h=1&x=1.0"
 		"UpgradeCheck"
 		"Device"
@@ -67,9 +67,9 @@ update_firmware() {
 		"0.0"
 		"N0"
 	)
-	address=$(printf "%%2F%s" "${factors[@]}" | cut -c4-)
-	address=$(curl -Ls "$address" | jq -r ".[0].body" | jq -r ".UpgradeURL")
-	archive=$(mktemp -d)/$(basename "$address")
+	local address=$(printf "%%2F%s" "${factors[@]}" | cut -c4-)
+	local address=$(curl -Ls "$address" | jq -r ".[0].body" | jq -r ".UpgradeURL")
+	local archive=$(mktemp -d)/$(basename "$address")
 	curl -Ls "$address" -o "$archive"
 	unzip -q "$archive" -d "$(access_external)/.kobo" 2>/dev/null
 
@@ -77,25 +77,25 @@ update_firmware() {
 
 update_koreader() {
 
-	tempdir=$(mktemp -d)
-	address="https://www.mobileread.com/forums/showpost.php?p=3797095&postcount=1"
-	pattern="href=\"\K(http(.*)OCP-KOReader(.*).zip)(?=\")"
-	address=$(curl -Ls "$address" | ggrep -oP "$pattern" | head -1)
-	archive=$tempdir/$(basename "$address")
+	local tempdir=$(mktemp -d)
+	local address="https://www.mobileread.com/forums/showpost.php?p=3797095&postcount=1"
+	local pattern="href=\"\K(http(.*)OCP-KOReader(.*).zip)(?=\")"
+	local address=$(curl -Ls "$address" | ggrep -oP "$pattern" | head -1)
+	local archive=$tempdir/$(basename "$address")
 	curl -Ls "$address" -o "$archive"
 	if [[ $(uname -s) = Dar* ]]; then
-		content=$(curl -Ls "https://www.mobileread.com/forums/showpost.php?p=3797096&postcount=2")
-		pattern="href=\"\K(http(.*)kfm_mac_install.zip)(?=\")"
-		address=$(echo "$content" | ggrep -oP "$pattern" | head -1)
-		archive=$tempdir/$(basename "$address")
+		local content=$(curl -Ls "https://www.mobileread.com/forums/showpost.php?p=3797096&postcount=2")
+		local pattern="href=\"\K(http(.*)kfm_mac_install.zip)(?=\")"
+		local address=$(echo "$content" | ggrep -oP "$pattern" | head -1)
+		local archive=$tempdir/$(basename "$address")
 		curl -Ls "$address" -o "$archive"
 		unzip -q "$archive" -d "$tempdir"
 		chmod +x "$tempdir/install.command" && echo 0 | "$tempdir/install.command" &>/dev/null
 	elif [[ $(uname -s) = Lin* ]]; then
-		content=$(curl -Ls "https://www.mobileread.com/forums/showpost.php?p=3797096&postcount=2")
-		pattern="href=\"\K(http(.*)kfm_nix_install.zip)(?=\")"
-		address=$(echo "$content" | grep -oP "$pattern" | head -1)
-		archive=$tempdir/$(basename "$address")
+		local content=$(curl -Ls "https://www.mobileread.com/forums/showpost.php?p=3797096&postcount=2")
+		local pattern="href=\"\K(http(.*)kfm_nix_install.zip)(?=\")"
+		local address=$(echo "$content" | grep -oP "$pattern" | head -1)
+		local archive=$tempdir/$(basename "$address")
 		curl -Ls "$address" -o "$archive"
 		unzip -q "$archive" -d "$tempdir"
 		chmod +x "$tempdir/install.sh" && echo 0 | "$tempdir/install.sh" &>/dev/null
